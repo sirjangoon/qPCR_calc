34af06e666d58a726da4e2bd19ce170005a7fc73
Harry M <15116239+sirjangoon@users.noreply.github.com>
2019-06-30 17:22:39 -0400

    changed input/outputs to xlsx

--- README.md
+++ README.md
@@ -2,14 +2,9 @@
 
 ## Todo
 
-- change input to excel
-- prompt to select input
 - addition calculations
 
 ## Instructions
 
-1. Create a TSV with columns 'Target Name', 'Sample Name', 'CT'
-2. Rename TSV to `sample_data.tsv`
-3. Place TSV in same directory as qPCR_calc.py
-4. Run `python3 qPCR_calc.py`
-5. Output is `out.tsv`
+1. Run `python3 qPCR_calc.py 'filename.xlsx' 'Optional_Control_Name'
+2. Output to 'CALCULATED_filename.xlsx'
--- qPCR_calc.py
+++ qPCR_calc.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
 '''
+This script takes raw output from qPCR and performs calculations on the results.
+
+Example:
+    $ qPCR_calc.py file_name.xlsx CONTROL_NAME
+
+Args:
+    file_name: raw output from qPCR in xlsx format
+    control_target: control unit to perform calculations against
+        default = "GAPDH"
+
 todo:
-Get flow data
-calculate 2^( (Flow_sample - Flow_gapdh) - (Sample_sample - Sample_gapdh) )
+    - Get flow data
+    - calculate 2^( (Flow_sample - Flow_gapdh) - (Sample_sample - Sample_gapdh) )
 '''
 
 import csv
-
-DATA_FILE = 'sample_data.tsv'
-CONTROL_TARGET = 'gapdh'
-SAMPLE_NAME = 'Sample Name'
-TARGET_NAME = 'Target Name'
-CT = 'CT'
-DELTA_CT = 'delta CT'
-AVERAGE_CT = 'average CT'
+import openpyxl
+import sys
 
 class SampleClass:
     def __init__(self, CT):
@@ -31,49 +35,100 @@ class SampleClass:
     def getCount(self):
         return self.count
 
+def openExcel(excel_workbook, control_target):
+    data_dict = {} 
+    wb_obj = openpyxl.load_workbook(excel_workbook)
+    sheet_obj = wb_obj['Results']
+    control_target_exist = False
 
+    if sheet_obj['D43'].value != 'Sample Name':
+        print('Results worksheet does not match expected layout. Exiting')
+        return 
+
+    for row_obj_tup in sheet_obj.iter_rows(min_row = 44,  min_col=4, max_col=15):
+        target = row_obj_tup[1].value
+        sample = row_obj_tup[0].value
+        ct = row_obj_tup[11].value
+
+        # stop if we reach line with no values
+        if target is None:
+            print('The last row is ' + str(row_obj_tup[0].row - 1) +'.')
+            break
+
+        # control_target case insensitive
+        if (not control_target_exist) and (target.upper() == control_target):
+            control_target = target
+            control_target_exist = True
+
+        try:
+            float(ct)
+        except:
+            print('Row ' + str(row_obj_tup[0].row) + ' does not have a numeric CT value. Row ignored.')
+            continue
+
+        #check if target exists in dict
+        if target in data_dict:
+            # check if sample exists in subdict
+            if sample in data_dict[target]:
+                data_dict[target][sample].add(ct)
+            else:
+                data_dict[target][sample] = SampleClass(ct)
+
+        else:
+            # initialize key with subdict of sample and SampleClass object
+            data_dict[target] = {sample: SampleClass(ct)}
+
+    if control_target_exist == False:
+        print('No control found in spreadsheet. No output created.')
+        return
+
+    control_dict = data_dict[control_target].copy()
+    writeExcel(excel_workbook, data_dict, control_dict, control_target)
+
+def writeExcel(file_name, data_dict, control_dict, control_target):
+    wb_obj = openpyxl.Workbook()
+    sheet_obj = wb_obj.active
+    sheet_obj.title = 'Calculations'
+
+    sheet_obj.cell(row = 1, column = 1).value = 'Target Name'
+    sheet_obj.cell(row = 1, column = 2).value = 'Sample Name'
+    sheet_obj.cell(row = 1, column = 3).value = 'Average CT'
+    sheet_obj.cell(row = 1, column = 4).value = 'Delta CT'
+
+    row_index = 2
+
+    for target_key in data_dict:
+        for sample_key in data_dict[target_key]:
+            ct_average = data_dict[target_key][sample_key].getAverage()
+            ct_delta = ct_average - control_dict[sample_key].getAverage()
+
+            # write
+            sheet_obj.cell(row = row_index, column = 1).value = target_key
+            sheet_obj.cell(row = row_index, column = 2).value = sample_key
+            sheet_obj.cell(row = row_index, column = 3).value = ct_average
+            sheet_obj.cell(row = row_index, column = 4).value = ct_delta
+
+            row_index += 1
+    
+    new_file = 'CALCULATED_' + file_name
+    wb_obj.save(new_file)
+    print('Created file ' + new_file)
 
 def main():
-    data_dict = {} 
-    with open(DATA_FILE) as tsvfile:
-        reader = csv.DictReader(tsvfile, dialect='excel-tab') 
-        for row in reader:
-            sample = row[SAMPLE_NAME]
-            target = row[TARGET_NAME]
-            ct = row[CT]
+    try:
+        file_location = sys.argv[1]
+        try:
+            control_target = sys.argv[2].upper() #eg, GAPDH, gapdh
+        except:
+            control_target = "GAPDH"
+        openExcel(file_location, control_target)
+    except IndexError:
+        print('Usage: python3 qPCR_calc.py FILENAME [gapdh]')
+        print()
+        print('Example: python3 qPCR_calc.py test_file.xlsx')
+        print('Example: python3 qPCR_calc.py test_file. xlsx GaPdH')
+    except openpyxl.utils.exceptions.InvalidFileException:
+        print('Only xlsx files are supported.')
 
-            # check if target exists
-            if target in data_dict:
-                # check if sample exists in subdict
-                if sample in data_dict[target]:
-                    data_dict[target][sample].add(ct)
-                else:
-                    data_dict[target][sample] = SampleClass(ct)
-
-            else:
-                # initialize key with subdict of sample and SampleClass object
-                data_dict[target] = {sample: SampleClass(ct)}
-
-            print(row)
-            print(row['Sample Name'])
-            if data_dict[target][sample].getCount() == 3:
-                print(data_dict[target][sample].getAverage())
-    
-    tsvfile.close()
-
-    control_dict = data_dict[CONTROL_TARGET].copy()
-    #print(control_dict)
-    #print(data_dict)
-
-    with open('out.tsv', 'w') as writetsv:
-        outwriter = csv.writer(writetsv, dialect='excel-tab')
-        outwriter.writerow([TARGET_NAME, SAMPLE_NAME, AVERAGE_CT, DELTA_CT])
-        # loop through dictionary
-        for target_key in data_dict:
-            for sample_key in data_dict[target_key]:
-                ct_average = data_dict[target_key][sample_key].getAverage()
-                ct_delta = ct_average - control_dict[sample_key].getAverage()
-                outwriter.writerow([target_key, sample_key, ct_average, ct_delta])
-    #dialect='excel-tab')
-
-main()
+if __name__ == '__main__':
+    main()
